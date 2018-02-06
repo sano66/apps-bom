c65334918e847a22abea1d9e91073e4a98cb7292
sano <sano66@gmail.com>
2018-02-06 20:23:50 +0900

    modify comment header

--- sso/src/main/java/com/example/apps/sso/config/security/MyFormLoginConfigurer.java
+++ sso/src/main/java/com/example/apps/sso/config/security/MyFormLoginConfigurer.java
@@ -1,3 +1,8 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package com.example.apps.sso.config.security;
 
 import org.springframework.security.config.annotation.web.HttpSecurityBuilder;
